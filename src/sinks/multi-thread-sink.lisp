45a95c90b559dd3b29a515fd213af9cded88ef8f
Ilya Khaprov <dead.trickster@gmail.com>
2015-12-10 02:25:48 +0300

    multi-thread-sink

--- src/sinks/multi-thread-sink.lisp
+++ src/sinks/multi-thread-sink.lisp
@@ -1,5 +1,24 @@
 (in-package :cl-events)
 
-(defclass multi-thread-sink ()
-  ()
-  (:documentation "vnethandlers added/removed possibly concurrently from many threads."))
+(defclass multi-thread-sink (#-cas
+                      single-thread-sink)
+  (#+cas
+   (proxy :initform (make-instance 'single-thread-sink))
+   #-cas
+   (lock :initform (bt:make-lock "multi-thread-sink lock")))
+  (:documentation "Hanldlers added/removed possibly concurrently from many threads."))
+
+#-cas
+(defmethod event-handlers-list :around ((sink multi-thread-sink))
+  (bt:with-lock-held ((slot-value sink 'lock))
+    (call-next-method)))
+
+#-cas
+(defmethod add-event-handler :around ((sink multi-thread-sink) handler &key &allow-other-keys)
+  (bt:with-lock-held ((slot-value sink 'lock))
+    (call-next-method)))
+
+#-cas
+(defmethod remove-event-handler :around ((sink multi-thread-sink) handler)
+  (bt:with-lock-held ((slot-value sink 'lock))
+    (call-next-method)))
