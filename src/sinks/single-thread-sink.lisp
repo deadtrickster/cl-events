2be7c41c14c2bd0240cc943008f2260debb727ed
Ilya Khaprov <dead.trickster@gmail.com>
2015-12-10 02:07:51 +0300

    single-thread-sink

--- src/sinks/single-thread-sink.lisp
+++ src/sinks/single-thread-sink.lisp
@@ -1,5 +1,19 @@
 (in-package :cl-events)
 
 (defclass single-thread-sink ()
-  ()
+  ((handlers :initform (list) :accessor sink-handlers)
+   (copy :initform (list) :accessor sink-handlers-copy))
   (:documentation "eventhandlers added/removed in single-threaded context."))
+
+(defmethod event-handlers-list ((sink single-thread-sink))
+  (sink-handlers-copy sink))
+
+(defmethod add-event-handler ((sink single-thread-sink) handler &key &allow-other-keys)
+  (setf (sink-handlers sink) (append (sink-handlers sink) (list handler))
+        (sink-handlers-copy sink) (copy-seq (sink-handlers sink)))
+  sink)
+
+(defmethod remove-event-handler ((sink single-thread-sink) handler)
+  (setf (sink-handlers sink) (remove handler (sink-handlers sink))
+        (sink-handlers-copy sink) (sink-handlers sink))
+  sink)
